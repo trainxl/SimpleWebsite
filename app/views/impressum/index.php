d50dfd5a1fb5c88d7a1c2f3d9ff348c655a161cf
trainxl <github@trainxl.de>
2011-06-07 22:01:43 +0200

    Little changes

--- app/views/impressum/index.php
+++ app/views/impressum/index.php
@@ -1,2 +1,3 @@
 <h2>Impressum</h2>
-<p>Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</p>
+<p>Nullam dictum felis eu pede mollis pretium.</p>
+<p>Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</p>
